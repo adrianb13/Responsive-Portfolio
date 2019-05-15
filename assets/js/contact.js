244abd090d33075a41a1ad0297dcfb68f4202d92
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-05-15 14:40:26 -0700

    update

---
+++ assets/js/contact.js
@@ -0,0 +1,46 @@
+let fullname = "";
+let contactemail = "";
+let contactphone = "";
+let contactmessage = "";
+
+$("#submit").on("click", function contact(event) {
+  event.preventDefault();
+  fullname = $("#fullname").val().trim();
+  contactemail = $("#email").val().trim();
+  contactphone = $("#phone").val().trim();
+  contactmessage = $("#message").val().trim();
+  console.log(fullname + "," + email + "," + message)
+  saveFirebase()
+  clear()
+  alert("Thank You! Your message has been sent!");
+})
+
+function saveFirebase () {
+  database.ref("/"+fullname).set({
+    name: fullname,
+    email: contactemail,
+    phone: contactphone,
+    message: contactmessage
+  });
+};
+
+function clear () {
+  $("#fullname").val("");
+  $("#email").val("");
+  $("#message").val("");
+}
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCzg29-VzhASBtvHlfDD3xuwEN1BuV-VhA",
+  authDomain: "profile-page-13e08.firebaseapp.com",
+  databaseURL: "https://profile-page-13e08.firebaseio.com",
+  projectId: "profile-page-13e08",
+  storageBucket: "profile-page-13e08.appspot.com",
+  messagingSenderId: "930966470683",
+  appId: "1:930966470683:web:8772c6e1f716024c"
+};
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+database.ref("/"+fullname);
